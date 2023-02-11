2d59d67cc6876967761917a1eeb71ebdd3a8774f
MilicaVa <dablbraids@gmail.com>
2023-02-11 23:25:12 +0100

    app.js rutiranje

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Search from './components/Search';
+
 
 function App() {
   return (
+
     <div className="App">
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </Router>
     </div>
+
   );
 }
 
